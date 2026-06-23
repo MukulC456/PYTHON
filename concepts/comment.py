a08517327c2a55ca4b0e683bb65e6fcf3fd9d676
MukulC456 <mchoudhari2004@gmail.com>
2026-06-23 22:10:09 +0530

    my work

---
+++ concepts/comment.py
@@ -0,0 +1,8 @@
+# Always use comment otherwise you will forget later on which line has error while debugging
+# Comment does not get printed
+
+# This is a comment
+
+'''
+This is a comment
+'''
---
+++ concepts/using_print.py
@@ -0,0 +1,15 @@
+# Single/Double quotes
+'''
+print("Hello, Mukul-double quotes") # I prefer this
+print('Hello, Mukul-single quotes')
+'''
+# Getting input from user
+'''
+number = int(input("Enter a number: "))
+print(f"Your number is {number}")
+'''
+#Printing blank lines
+print("hello")
+print() # prints nothing
+print("Mukul")
+print("This is first line \nThis is second line")
