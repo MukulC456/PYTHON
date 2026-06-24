38e0fe91812297f469c1618120256a5b921aa477
MukulC456 <mchoudhari2004@gmail.com>
2026-06-24 12:20:28 +0530

    my commits

---
+++ concepts/numbers.py
@@ -0,0 +1,19 @@
+'''
+first_num = input("Enter first number: ")
+second_num = input("Enter second number: ")
+
+print(float(first_num)+float(second_num))
+print(int(first_num)+int(second_num))
+'''
+# By default, the input for numbers is in int format. Use float to ask decimal input.
+'''
+first_num = float(input("Enter first number: "))
+second_num = float(input("Enter second number: "))
+
+print(float(first_num)+float(second_num))
+print(int(first_num)+int(second_num))
+'''
+
+months = 12
+print(f"There are {months} months in a year.")
+print("There are "+str(months)+" months in a year.")
---
+++ concepts/strings.py
@@ -0,0 +1,21 @@
+# Combining strings
+'''
+first_name = "Mukul"
+last_name = "Choudhari"
+print(f"Hello, {first_name} {last_name}")
+'''
+
+# string functions
+'''
+sentence = "this is an Example Statement."
+
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.capitalize())
+print(sentence.count('e'))
+'''
+
+# Formatted String
+color = "black"
+vehicle = "car"
+print(f"I saw a {color} {vehicle}")
