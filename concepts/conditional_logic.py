83064d43fd4f87848c350b0b845275c1c319e664
MukulC456 <mchoudhari2004@gmail.com>
2026-06-26 16:44:24 +0530

    conditional logic

--- concepts/conditional_logic.py
+++ concepts/conditional_logic.py
@@ -79,3 +79,30 @@ else:
     tax = 0
     print(f"Tax is {tax}%")
 '''
+
+# using boolean to make normal code efficient
+'''
+A student makes honour roll if their GPA is >= 8.5 and their lowest grade is not below 7.5
+'''
+
+'''
+# normal code
+gpa = float(input("Enter you GPA: "))
+lowest_grade = float(input("Enter your lowest grade: "))
+
+if gpa >= 8.5 and lowest_grade >= 7.5:
+    print("You made the honour roll")
+'''
+'''
+# using boolean for normal code
+gpa = float(input("Enter you GPA: "))
+lowest_grade = float(input("Enter your lowest grade: "))
+
+if gpa >= 8.5 and lowest_grade >= 7.5:
+    honour_roll = True
+else:
+    honour_roll = False
+
+if honour_roll:                  # We can use this to prevent writing the above if logic again and again
+    print("You made honour_roll") # We can recall the condition using boolean and print the output.
+'''
