8c4207f0847694acff6a8abc1850287661d7663f
MukulC456 <mchoudhari2004@gmail.com>
2026-06-28 20:34:20 +0530

    mod and pack

---
+++ concepts/modules_and_packages.py
@@ -0,0 +1,6 @@
+'''
+Module: A python file with functions, classes and other components. We use it to break down code into
+        reusable structures.
+
+Package: Published collection of module
+'''
