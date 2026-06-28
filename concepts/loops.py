4ae7c418683d695b6cc709e7a4b12d4744c46625
MukulC456 <mchoudhari2004@gmail.com>
2026-06-28 10:52:46 +0530

    loops

---
+++ concepts/loops.py
@@ -0,0 +1,14 @@
+'''
+people = ["Mukul","Khush","Aryan"]
+
+for name in people:
+    print(name)
+'''
+'''
+people = ["Mukul","Aryan","Khush"]
+
+index = 0
+while index < len(people):
+    print(people[index])
+    index = index + 1
+'''
