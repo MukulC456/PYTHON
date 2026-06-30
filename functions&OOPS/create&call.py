7cbf69e95765f8118277e5f2ff6f02678c65b07f
MukulC456 <mchoudhari2004@gmail.com>
2026-06-30 17:39:36 +0530

    create&call

---
+++ functions&OOPS/create&call.py
@@ -0,0 +1,14 @@
+# how to create and call a func
+# creating funcs greet() and name()
+def greet():
+    print("Hello")
+    print("Welcome")
+
+def name():
+    print("Mukul")
+
+# calling func whenever u want (without calling output will be blank, nothing will be printed)
+greet()
+name()
+name()
+greet()
