b1510d2eacf58d98b4da0725ad9d1fd06c2482b0
MukulC456 <mchoudhari2004@gmail.com>
2026-07-01 17:50:05 +0530

    def args

---
+++ functions&OOPS/default_arguments.py
@@ -0,0 +1,9 @@
+def add(num1=10,num2=5): 
+    total = num1+num2
+    return total
+
+x = add()
+print(x)
+
+# default values can be set here for parameters like num1 and num2
+# if we give only one default vale in line1 then we have to pass the value of another in line5.
